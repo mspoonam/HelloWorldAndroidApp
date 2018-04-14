6a935a8bc4f2c2aca71c330c8ffba8ff0ea4c253
mspoonam <35603752+mspoonam@users.noreply.github.com>
2018-04-15 04:58:06 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/sanketb/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sanketb/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sanketb.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// MainActvoty 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
