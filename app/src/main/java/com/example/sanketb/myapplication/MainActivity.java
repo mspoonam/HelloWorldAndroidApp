d656dcf91becd87a984b6ee6eb80e7d9952e1543
mspoonam <35603752+mspoonam@users.noreply.github.com>
2018-04-16 15:08:11 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/sanketb/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sanketb/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 // MainActvity Class  
 
-public class MainActivity1 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
