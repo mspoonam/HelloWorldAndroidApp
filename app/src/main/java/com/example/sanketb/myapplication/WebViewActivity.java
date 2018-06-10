547d06c23340c794392ccf6e18661a69b50c609e
Poonam Panday <mspoonam.contact@gmail.com>
2018-06-10 17:41:17 +0530

    Added Activity name inside the textview
    Created WebviewActivity
    created design xml file for webview
    for all the clicks connected to werbview acitivity class

---
+++ app/src/main/java/com/example/sanketb/myapplication/WebViewActivity.java
@@ -0,0 +1,23 @@
+package com.example.sanketb.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends Activity {
+
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl(getIntent().getExtras().getString("keyName"));
+
+
+    }
+
+}
